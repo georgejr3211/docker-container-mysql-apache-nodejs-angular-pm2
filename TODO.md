2e9194977f4210c8491d207eda183f04adc40b57
George Alexandre <georgefeitosajr12@gmail.com>
2019-12-16 16:14:30 -0300

    Docker container - MySQL, NodeJS, Angular, PM2, Apache

--- TODO.md
+++
@@ -1,3 +0,0 @@
-[X] - Conexao com o banco de dados
-[X] - Iniciar a API ao dar build - 13:56
-[X] - Melhorar o Dockerfile para que nao seja necessario informar o nome das aplicacoes
--- script.sh
+++ script.sh
@@ -1,11 +1,40 @@
 MAIN_DIR=/var/www
 
-a2dissite 000-default.conf && a2ensite \
-  app.com.conf \
-  api.com.conf
+API_NAME=fairtek-server 
 
-echo "RESTARTANDO APACHE"
+APP_NAME=fairtek-portal
+
+API_CONF=fairtek-server.com.conf
+
+APP_CONF=fairtek-portal.com.conf
+
+PM2_START_FILENAME=pm2.json
+
+LOG_BACKUP_FILENAME=logs_backup.txt
+
+DUMP_DAYS_FOLDER=dump_days
+
+a2dissite 000-default.conf && a2ensite \
+  $APP_CONF $APP_CONF
+
 service apache2 restart
 
-echo "Inciando a API"
-cd $MAIN_DIR && pm2-docker pm2.json
+mkdir $MAIN_DIR/$API_NAME/$DUMP_DAYS_FOLDER
+
+if [ ! -e $MAIN_DIR/$API_NAME/$LOG_BACKUP_FILENAME ]; then
+  cd $MAIN_DIR/$API_NAME
+  touch $LOG_BACKUP_FILENAME
+fi
+
+cd $MAIN_DIR/$APP_NAME
+npm i
+npm audit fix
+npm update
+ng build --prod
+
+cd $MAIN_DIR/$API_NAME
+npm i
+npm audit fix
+
+cd $MAIN_DIR
+pm2-docker $PM2_START_FILENAME
